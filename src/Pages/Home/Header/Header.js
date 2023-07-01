7788b3cfa99ad3d09008438f813cde2c38de97dc
sabrina <sabrinaahmednitu@gmail.com>
2023-07-02 00:50:50 +0600

    hero done

--- src/Pages/Home/Header/Header.js
+++ src/Pages/Home/Header/Header.js
@@ -1,38 +1,40 @@
 import React from 'react';
-import {
-  Container,
-  Form,
-  Nav,
-  Navbar,
-} from 'react-bootstrap';
+import { Container, Form, Nav, Navbar } from 'react-bootstrap';
 import icon from '../../../images/logo.png';
 import './Header.css';
 
 const Header = () => {
   return (
-    <div className='container' >
-      <Navbar expand="lg" >
+    <div className="main-header">
+      <Navbar expand="lg">
         <Container fluid>
           <Navbar.Brand href="#">
             <img className="nav-img" src={icon} alt="" />
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
-            <Form className="search">
-              <Form.Control
+            <Form className="search d-flex">
+              <i className="searchIcon fa-sharp fa-solid fa-magnifying-glass"></i>
+              <input
+                className="searchBox"
                 type="search"
-                placeholder="Search"
-                className="me-2"
-                aria-label="Search"
+                placeholder="Search your destination"
+                style={{
+                  backgroundColor: 'transparent',
+                }}
               />
-            </Form >
-            <Nav className="me-auto" style={{ maxHeight: '100px' }} navbarScroll>
+            </Form>
+            <Nav
+              className="me-auto"
+              style={{ maxHeight: '100px' }}
+              navbarScroll
+            >
               <Nav.Link href="#action1">News</Nav.Link>
               <Nav.Link href="#action2">Destination</Nav.Link>
               <Nav.Link href="#action1">Blog</Nav.Link>
               <Nav.Link href="#action2">Contact</Nav.Link>
             </Nav>
-            <button>Login</button>
+            <button className="">Login</button>
           </Navbar.Collapse>
         </Container>
       </Navbar>
--- src/Pages/Home/Hero/Hero.js
+++ src/Pages/Home/Hero/Hero.js
@@ -1,19 +1,23 @@
 import React from 'react';
-import './Hero.css'
+import './Hero.css';
+import hero from '../../../images/Rectangle 28.png'
 
 const Hero = () => {
     return (
       <div>
         <div className="hero">
-          <div className="container">
-            <div className="row">
+          <div className="hero-container container">
+            <div className="hero-text">
               <h1>COX'S BAZAR</h1>
               <p>
                 Cox's Bazar is a city ,fishing port ,tourism center and district
                 headquarters in southeastern Bangladesh.It is famous mostly for
                 its long natural sandy beach,and it...
               </p>
-              <button> Booking <i class="fa-solid fa-arrow-right"></i> </button>
+              <button className='button' > Booking <i class="fa-solid fa-arrow-right"></i> </button>
+            </div>
+            <div className="hero-img">
+              <img src={hero} alt="" />
             </div>
           </div>
         </div>
